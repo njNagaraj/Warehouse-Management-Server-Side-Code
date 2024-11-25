16e18d152de4ce5d8fa92f926c512d007ed2c879
Nj <njraj24@gmail.com>
2024-11-25 15:10:00 +0530

    Modified

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get('/orders', (req, res) => {
     const query = 'SELECT * FROM order_table';
     db.query(query, (err, result) => {
         if (err) {
             console.error('Database Query Error: ', err.message);
-            res.status(500).json({ error: err.message }); // Include error details in the response
+            res.status(500).json({ error: err.message }); 
             return;
         }
         res.json(result);
